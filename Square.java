cf6193dbfa3ba0d3d62abb6da990c6ca6e7017c3
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-16 12:34:44 +0600

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,12 @@
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+}
